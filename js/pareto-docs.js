9adbafb9bb23b07976dd7cbead7b07d9f974d7df
jeffyactive <jeff@reelyactive.com>
2017-11-08 15:56:44 -0500

    Initial commit

---
+++ js/pareto-docs.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright reelyActive 2017
+ * We believe in an open Internet of Things
+ */
+
+angular.module('pareto-docs', [ 'ui.bootstrap' ])
+
+  // Interaction controller
+  .controller('InteractionCtrl', function($scope) {
+
+  });
